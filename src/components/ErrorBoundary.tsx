435a2a56ae22857a5d79af83eb1c361aaf7cc4e0
3BD ELATTEF <weeessd57@gmail.com>
2025-08-29 11:11:49 +0300

    fix vercel 2

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -26,10 +26,6 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   };
   private retryTimeoutId: ReturnType<typeof setTimeout> | null = null;
 
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-  }
-
   static getDerivedStateFromError(error: Error): Partial<ErrorBoundaryState> {
     return {
       hasError: true,
